84332b3891b8b3b59f1c7e9139f27c2f3d62e7ca
smlnkls1208 <al89994950059@gmail.com>
2025-10-04 10:43:59 +0700

    сохранили имя

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,23 @@
-console.log("game")
+class Game {
+    constructor() {
+        this.name = name;
+    }
+    start () {
+        this.loop()
+    }
+
+    loop() {
+        requestAnimationFrame(() => {
+            this.setParams();
+            this.loop()
+        });
+    }
+
+    setParams() {
+        let params = ['name'];
+        let values = [this.name];
+        params.forEach((param,ind) => {
+            $(`#${param}`).innerHTML = values[ind];
+        })
+    }
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,63 @@
-console.log("main")
+let name = '';
+let game = {};
+let panel = 'start';
+let $ = function (domElement) {
+    return document.querySelector(domElement);
+}
+
+let nav = () => {
+    document.onclick = (e) => {
+        e.preventDefault();
+        switch (e.target.id) {
+            case 'startGame':
+                go('game', 'd-flex');
+                break;
+            case 'restart':
+                go('game', 'd-flex');
+                break;
+        }
+    }
+}
+
+let go = (page, attribute) => {
+    let pages = ['start', 'game', 'end'];
+    panel = page;
+    $(`#${ page }`).setAttribute('class', attribute);
+    pages.forEach(el => {
+        if(el !== page) $(`#${el}`).setAttribute('class', 'd-none');
+    })
+}
+
+let checkStorage = () => {
+    $('#nameInput').value = localStorage.getItem('userName') || '';
+}
+
+let checkName = () => {
+    name = $('#nameInput').value.trim();
+    if(name !== '') {
+        localStorage.setItem('userName', name);
+        $('#startGame').removeAttribute('disabled');
+    } else {
+        $('#startGame').setAttribute('disabled', '');
+    }
+}
+
+let startLoop = () => {
+    let inter = setInterval(() => {
+        checkName();
+        if(panel !== 'start') clearInterval(inter);
+    }, 100)
+}
+
+window.onload = () => {
+    checkStorage();
+    nav();
+    startLoop();
+    setInterval(() => {
+        if(panel === 'game') {
+            game = new Game();
+            game.start();
+            panel = 'game process';
+        }
+    }, 500)
+}
