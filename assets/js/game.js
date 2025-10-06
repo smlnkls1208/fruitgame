a339f1f1cd418e054b7383533461a3c445cfea6b
smlnkls1208 <al89994950059@gmail.com>
2025-10-06 11:45:47 +0700

    add collision

--- assets/js/game.js
+++ assets/js/game.js
@@ -30,6 +30,61 @@ class Drawable {
             height: ${this.h}px;
         `;
     }
+
+    isCollision(element) {
+        let a = {
+            x1: this.x,
+            y1: this.y,
+            x2: this.x + this.w,
+            y2: this.y + this.h
+        };
+        let b = {
+            x1: element.x,
+            y1: element.y,
+            x2: element.x + element.w,
+            y2: element.y + element.h
+        }
+        return a.x1 < b.x2 && b.x1 < a.x2 && a.y1 < b.y2 && b.y1 < a.y2;
+    }
+
+}
+
+class Fruit extends Drawable {
+    constructor(game) {
+        super(game);
+        this.w = 70;
+        this.h = 70;
+        this.y = 60;
+        this.x = random(0, window.innerWidth - this.w);
+        this.offsets.y = 3;
+        this.createElement();
+    }
+
+    update(){
+        if(this.isCollision(this.game.player)) console.log('plus point');
+        if(this.y > window.innerHeight) console.log('minus hp');
+        super.update();
+    }
+}
+
+class Banana extends Fruit {
+    constructor(game) {
+        super(game);
+    }
+}
+
+class Apple extends Fruit {
+    constructor(game) {
+        super(game);
+        this.offsets.y = 5;
+    }
+}
+
+class Orange extends Fruit {
+    constructor(game) {
+        super(game);
+        this.offsets.y = 7;
+    }
 }
 
 class Player extends Drawable {
@@ -72,6 +127,8 @@ class Game {
         this.name = name;
         this.elements = [];
         this.player = this.generate(Player);
+        this.counterForTimer = 0;
+        this.fruits = [Apple, Banana, Orange];
     }
 
     start () {
@@ -86,10 +143,18 @@ class Game {
 
     loop() {
         requestAnimationFrame(() => {
+            this.counterForTimer++;
+            if (this.counterForTimer % 60 === 0) {
+                this.randomFruitGenerate();
+            }
             this.updateElements();
             this.setParams();
             this.loop();
         });
+    }
+
+    randomFruitGenerate() {
+        this.generate(this.fruits[random(0, 2)]);
     }
 
     updateElements() {
--- assets/js/main.js
+++ assets/js/main.js
@@ -61,3 +61,9 @@ window.onload = () => {
         }
     }, 500)
 }
+
+let random = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
